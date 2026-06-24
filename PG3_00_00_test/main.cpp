68fe176af9bbce875bfc15d605ba2ac32498ffec
k025g1024 <k025g1024@g.neec.ac.jp>
2026-06-24 13:55:17 +0900

    PG3_01_02

--- PG3_00_00_test/main.cpp
+++ PG3_00_00_test/main.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
 
+
+#include <stdio.h>
+#include <windows.h>
 
 // 再帰的な賃金体系の時給計算関数
 int RecursiveWage(int n) {
@@ -8,28 +10,33 @@ int RecursiveWage(int n) {
     }
 
     return RecursiveWage(n - 1) * 2 - 50;
-    }
-
+}
 
 int main() {
+    // 文字化け
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     int hour;
-    int normalTotal;
+    int normalTotal = 0;
     int recursiveTotal = 0;
 
     printf("働く時間を入力してください：");
     scanf_s("%d", &hour);
 
-    // 一般的な賃金体系の合計
-    normalTotal = 1072 * hour;
+    printf("\n計算結果\n");
+    printf("------------------------------------------\n");
+    printf("時間\t一般的な賃金体系\t再帰的な賃金体系\n");
+    printf("------------------------------------------\n");
 
-    // 再帰的な賃金体系の合計
     for (int i = 1; i <= hour; i++) {
+        normalTotal = 1072 * i;
         recursiveTotal += RecursiveWage(i);
+
+        printf("%d時間\t%d円\t\t%d円\n", i, normalTotal, recursiveTotal);
     }
 
-    printf("\n%d時間働いた場合\n", hour);
-    printf("一般的な賃金体系：%d円\n", normalTotal);
-    printf("再帰的な賃金体系：%d円\n", recursiveTotal);
+    printf("------------------------------------------\n");
 
     if (normalTotal < recursiveTotal) {
         printf("再帰的な賃金体系のほうが稼げます。\n");
