d2e0bc4b6d2cae5c04830cbc718906bb492abb15
k025g1024 <k025g1024@g.neec.ac.jp>
2026-06-24 14:15:22 +0900

    PG3_01_03

--- PG3_00_00_test/PG3_00_01.cpp
+++ PG3_00_00_test/PG3_00_01.cpp
@@ -1,20 +1,98 @@
-// PG3_00_00_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
+#include <chrono>
 #include <iostream>
+#include <limits>
+#include <random>
+#include <thread>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+// 判定結果を受け取る
+using ResultCallback = void (*)(int);
+
+void ShowCorrectResult(int diceResult)
+{
+	std::cout << "\nサイコロの出目は " << diceResult << " です。\n";
+	std::cout << "正解\n";
+}
+
+void ShowIncorrectResult(int diceResult)
+{
+	std::cout << "\nサイコロの出目は " << diceResult << " です。\n";
+	std::cout << "不正解\n";
+}
+
+// 関数呼び出す
+void JudgeAnswer(
+	int diceResult,
+	int userAnswer,
+	ResultCallback onCorrect,
+	ResultCallback onIncorrect)
+{
+	const int correctAnswer = (diceResult % 2 == 0) ? 2 : 1;
+
+	if (userAnswer == correctAnswer) {
+		onCorrect(diceResult);
+	} else {
+		onIncorrect(diceResult);
+	}
+}
+
+int GetUserAnswer()
+{
+	int answer = 0;
+
+	while (true) {
+		std::cout << "出目は奇数と偶数のどちらでしょう？\n";
+		std::cout << "奇数なら 1、偶数なら 2 を入力してください: ";
+
+		if (std::cin >> answer && (answer == 1 || answer == 2)) {
+			return answer;
+		}
+
+		std::cout << "1 または 2 を入力してください。\n\n";
+		std::cin.clear();
+		std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+	}
+}
+
+int RollDice()
+{
+	std::random_device randomDevice;
+	std::mt19937 randomEngine(randomDevice());
+	std::uniform_int_distribution<int> dice(1, 6);
+	return dice(randomEngine);
+}
+
+void WaitForResult()
+{
+	std::cout << "判定中";
+
+	for (int i = 0; i < 3; ++i) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::cout << "." << std::flush;
+	}
+}
 
 int main()
 {
-    std::cout << "群星の旅へ\n";
+#ifdef _WIN32
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+#endif
+
+	const int diceResult = RollDice();
+	const int userAnswer = GetUserAnswer();
+
+	WaitForResult();
+
+	// コールバック
+	JudgeAnswer(
+		diceResult,
+		userAnswer,
+		ShowCorrectResult,
+		ShowIncorrectResult);
+
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
